40da6a1d0d4f8733df6795ecc043c911f5ce5d25
Alan Kelly <alan.kelly@maersk.com>
2024-11-24 11:05:24 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alan-kelly-maersk/stomp-client
+
+go 1.23
+
+require github.com/go-stomp/stomp/v3 v3.1.3
---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+kubectl delete -f deploy.yaml
+podman exec -it kind-control-plane /usr/local/bin/crictl rmi localhost/stomper:latest
+podman build -t stomper:latest .
+podman save localhost/stomper:latest -o image.tar
+kind load image-archive image.tar
+rm image.tar
+podman exec -it kind-control-plane /usr/local/bin/crictl images | grep stomper
+kubectl apply -f deploy.yaml
+kubectl logs stomper-0
